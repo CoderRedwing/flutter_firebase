b767deafa1b3784974dfb0f618c48061372f4ee3
CoderRedwing <ajiteshru@gmail.com>
2023-01-30 21:24:23 +0530

    add

--- lib/screen/login_scr.dart
+++ lib/screen/login_scr.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-  @override
+  @ override
   Widget build(BuildContext context) {
     return Scaffold(
        drawer: Drawer(
