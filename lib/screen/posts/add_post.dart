b131a1610065b7f212032b1aa63c88057f25b0d3
CoderRedwing <ajiteshru@gmail.com>
2023-02-03 15:04:13 +0530

    st

--- lib/screen/posts/add_post.dart
+++ lib/screen/posts/add_post.dart
@@ -1,3 +1,6 @@
+import 'package:firebase/utils/utils.dart';
+import 'package:firebase/widgets/round_button.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +12,57 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  final postController = TextEditingController();
+  bool loading = false;
+  final databaseRef = FirebaseDatabase.instance.ref('Test');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add post'),
+        title: const Text('Add post'),
       ),
-      body: Column(
-        children: [
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Column(
+          children:  [
+              const SizedBox(
+                height: 30,
+              ),
+            TextFormField(
+              maxLines: 4,
+              controller: postController,
+              decoration: const InputDecoration(
+                hintText: 'What in your minds',
+                border: OutlineInputBorder(),
 
-        ],
+              ),
+            ),
+            const SizedBox(height: 35,),
+            RoundButton(title: 'Add',loading: loading, onTap: (){
+              setState(() {
+                loading=true;
+              });
+                 databaseRef.child('ajitesh').set({
+
+                   'descr':postController.text.toString(),
+                   'aj':1
+
+                 }).then((value) {
+
+                   Utils().toastMessage('Post added');
+                   setState(() {
+                     loading=false;
+                   });
+                 }).onError((error, stackTrace){
+
+                   Utils().toastMessage(error.toString());
+                   setState(() {
+                     loading=false;
+                   });
+                 });
+            }),
+          ],
+        ),
       ),
     );
   }
